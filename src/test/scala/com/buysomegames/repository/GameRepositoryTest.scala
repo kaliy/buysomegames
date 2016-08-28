72b384370a2d72ad5e545fb78c17c284f81bc0ba
Arthur Kalimullin <kalimullin@gmail.com>
2016-08-28 21:16:43 +0200

    using scalatest matchers instead of assert #4

--- src/test/scala/com/buysomegames/repository/GameRepositoryTest.scala
+++ src/test/scala/com/buysomegames/repository/GameRepositoryTest.scala
@@ -1,6 +1,7 @@
 package com.buysomegames.repository
 
 import com.buysomegames.kernel.MongoConnectionModule
+import com.buysomegames.model.Game
 import com.buysomegames.test.FreshDatabase
 import com.twitter.inject.app.TestInjector
 import com.twitter.inject.{Injector, IntegrationTest}
@@ -10,7 +11,9 @@ class GameRepositoryTest extends IntegrationTest with FreshDatabase {
 
   "GameRepository.findAllGames" should {
     "return all available games" in {
-      assert(injector.instance[GameRepository].findAllGames.size == 2)
+      val games: Iterable[Game] = injector.instance[GameRepository].findAllGames
+      games should have size 2
+      games.map(_.name) should contain only ("Uncharted: Drake’s Fortune", "Gravity Rush")
     }
   }
 }
