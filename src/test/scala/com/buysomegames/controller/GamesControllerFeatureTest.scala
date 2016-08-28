9b42da8e4a63a0cd8548b4cd0e69aac1ac3b726f
Arthur Kalimullin <kalimullin@gmail.com>
2016-08-28 20:56:23 +0200

    Extracting database refresh into trait #4

--- src/test/scala/com/buysomegames/controller/GamesControllerFeatureTest.scala
+++ src/test/scala/com/buysomegames/controller/GamesControllerFeatureTest.scala
@@ -1,19 +1,14 @@
 package com.buysomegames.controller
 
-import java.util.concurrent.TimeUnit
-
 import com.buysomegames.kernel.BuysomegamesServer
+import com.buysomegames.test.FreshDatabase
 import com.twitter.finagle.http.Status
 import com.twitter.finatra.http.EmbeddedHttpServer
 import com.twitter.inject.server.FeatureTest
-import org.mongodb.scala.MongoDatabase
-import org.mongodb.scala.bson.collection.immutable.Document
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 import scala.io.Source
 
-class GamesControllerFeatureTest extends FeatureTest {
+class GamesControllerFeatureTest extends FeatureTest with FreshDatabase {
   override protected def server: EmbeddedHttpServer = new EmbeddedHttpServer(twitterServer = new BuysomegamesServer)
 
   "/games endpoint" should {
@@ -24,12 +19,5 @@ class GamesControllerFeatureTest extends FeatureTest {
         withJsonBody = Source.fromInputStream(getClass.getResourceAsStream("/controller/games/games.json")).getLines.mkString
       )
     }
-  }
-
-  override protected def beforeEach(): Unit = {
-    val collection = injector.instance[MongoDatabase].getCollection("games")
-    val lines = Source.fromInputStream(getClass.getResourceAsStream("/data/games.json")).getLines
-    Await.result(collection.drop().head(), Duration(10, TimeUnit.SECONDS))
-    Await.result(collection.insertMany(lines.map(json => Document(json)).toSeq).toFuture(), Duration(10, TimeUnit.SECONDS))
   }
 }
--- src/test/scala/com/buysomegames/repository/GameRepositoryTest.scala
+++ src/test/scala/com/buysomegames/repository/GameRepositoryTest.scala
@@ -1,31 +1,16 @@
 package com.buysomegames.repository
 
-import java.util.concurrent.TimeUnit
-import javax.inject.Inject
-
 import com.buysomegames.kernel.MongoConnectionModule
+import com.buysomegames.test.FreshDatabase
 import com.twitter.inject.app.TestInjector
 import com.twitter.inject.{Injector, IntegrationTest}
-import org.mongodb.scala.MongoDatabase
-import org.mongodb.scala.bson.collection.immutable.Document
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
-import scala.io.Source
-
-class GameRepositoryTest extends IntegrationTest {
+class GameRepositoryTest extends IntegrationTest with FreshDatabase {
   override protected def injector: Injector = TestInjector(modules = Seq(MongoConnectionModule))
 
   "GameRepository.findAllGames" should {
     "return all available games" in {
       assert(injector.instance[GameRepository].findAllGames.size == 2)
     }
-  }
-
-  override protected def beforeEach(): Unit = {
-    val collection = injector.instance[MongoDatabase].getCollection("games")
-    val lines = Source.fromInputStream(getClass.getResourceAsStream("/data/games.json")).getLines
-    Await.result(collection.drop().head(), Duration(10, TimeUnit.SECONDS))
-    Await.result(collection.insertMany(lines.map(json => Document(json)).toSeq).toFuture(), Duration(10, TimeUnit.SECONDS))
   }
 }
