296fb6591973f530042613125464c91371efc88e
Artur Kalimullin <kalimullin@gmail.com>
2017-02-06 14:23:11 +0100

    Injecting into the database happens only before each test and that's why retrieving data from DB in should clause won't work. Will refactor it later, for now will just read the date in each test. #7

--- src/test/scala/com/buysomegames/repository/GameEditionRepositoryIntegrationTest.scala
+++ src/test/scala/com/buysomegames/repository/GameEditionRepositoryIntegrationTest.scala
@@ -16,30 +16,43 @@ import scala.concurrent.{Await, Future}
 class GameEditionRepositoryIntegrationTest extends IntegrationTest with FreshDatabase {
   override protected def injector: Injector = TestInjector(modules = Seq(MongoConnectionModule))
 
-  "GameEditionsRepository" when {
-    "called findAllGameEditions method" should {
-      val gameEditionsFuture: Future[Iterable[GameEdition]] = injector.instance[GameEditionRepository].findAllGameEditions
-      val gameEditions: Iterable[GameEdition] = Await.result(gameEditionsFuture, Duration(10, TimeUnit.SECONDS))
-      val uncharted: GameEdition = gameEditions.find(_.id == new ObjectId("5894944f5591d01832058cda")).get
-      "return all available game editions" in {
-        gameEditions should have size 3
-      }
-      "map GameEdition.name" in {
-        uncharted.name should be ("Uncharted: Drake's Fortune")
-      }
-      "map GameEdition.gameId" in {
-        uncharted.gameId should be (new ObjectId("57c28e830868729b8638fe5c"))
-      }
-      "map GameEdition.name as test entity" in {
-        uncharted.platform.id should be ("PS3")
-        uncharted.platform.name should be ("changeit")
-      }
-      "map GameEdition.region from the database" in {
-        uncharted.region should be ("US")
-      }
-      "map GameEdition.releaseDate from the database" in {
-        uncharted.releaseDate should be (LocalDate.parse("2007-11-16"))
-      }
+  "GameEditionsRepository.findAllGameEditions method" should {
+    "return all available game editions" in {
+      val gameEditions = getAllGameEditions
+      gameEditions should have size 3
     }
+    "map GameEdition.name" in {
+      val uncharted = getAllGameEditionsAndExtractUncharted
+      uncharted.name should be("Uncharted: Drake's Fortune")
+    }
+    "map GameEdition.gameId" in {
+      val uncharted = getAllGameEditionsAndExtractUncharted
+      uncharted.gameId should be(new ObjectId("57c28e830868729b8638fe5c"))
+    }
+    "map GameEdition.name as test entity" in {
+      val uncharted = getAllGameEditionsAndExtractUncharted
+      uncharted.platform.id should be("PS3")
+      uncharted.platform.name should be("changeit")
+    }
+    "map GameEdition.region from the database" in {
+      val uncharted = getAllGameEditionsAndExtractUncharted
+      uncharted.region should be("US")
+    }
+    "map GameEdition.releaseDate from the database" in {
+      val uncharted = getAllGameEditionsAndExtractUncharted
+      uncharted.releaseDate should be(LocalDate.parse("2007-11-16"))
+    }
+  }
+
+  private def getAllGameEditionsAndExtractUncharted: GameEdition = {
+    val gameEditions: Iterable[GameEdition] = getAllGameEditions
+    val uncharted: GameEdition = gameEditions.find(_.id == new ObjectId("5894944f5591d01832058cda")).get
+    uncharted
+  }
+
+  private def getAllGameEditions: Iterable[GameEdition] = {
+    val gameEditionsFuture: Future[Iterable[GameEdition]] = injector.instance[GameEditionRepository].findAllGameEditions
+    val gameEditions: Iterable[GameEdition] = Await.result(gameEditionsFuture, Duration(10, TimeUnit.SECONDS))
+    gameEditions
   }
 }
