b5b64a0d59ac3a96d375a5435f512450382eaa6c
Artur Kalimullin <kalimullin@gmail.com>
2017-02-07 10:59:56 +0100

    ObjectId JSON serialization #7

--- src/main/scala/com/buysomegames/kernel/BuysomegamesJacksonModule.scala
+++ src/main/scala/com/buysomegames/kernel/BuysomegamesJacksonModule.scala
@@ -7,10 +7,17 @@ import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.databind.{JsonSerializer, SerializerProvider}
 import com.twitter.finatra.json.modules.FinatraJacksonModule
+import org.bson.types.ObjectId
 
-class LocalDateSerializer extends JsonSerializer[LocalDate] {
+private class LocalDateSerializer extends JsonSerializer[LocalDate] {
   override def serialize(value: LocalDate, gen: JsonGenerator, serializers: SerializerProvider): Unit = {
     gen.writeString(value.format(DateTimeFormatter.ISO_DATE))
+  }
+}
+
+private class ObjectIdSerializer extends JsonSerializer[ObjectId] {
+  override def serialize(value: ObjectId, gen: JsonGenerator, serializers: SerializerProvider): Unit = {
+    gen.writeString(value.toHexString)
   }
 }
 
@@ -18,6 +25,9 @@ object BuysomegamesJacksonModule extends FinatraJacksonModule {
   override val additionalJacksonModules = Seq(
     new SimpleModule {
       addSerializer(classOf[LocalDate], new LocalDateSerializer)
+    },
+    new SimpleModule {
+      addSerializer(classOf[ObjectId], new ObjectIdSerializer)
     }
   )
 }
--- src/test/scala/com/buysomegames/kernel/BuysomegamesJacksonModuleTest.scala
+++ src/test/scala/com/buysomegames/kernel/BuysomegamesJacksonModuleTest.scala
@@ -4,6 +4,7 @@ import java.time.LocalDate
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.twitter.inject.app.TestInjector
+import org.bson.types.ObjectId
 import org.scalatest.{Matchers, WordSpec}
 
 class BuysomegamesJacksonModuleTest extends WordSpec with Matchers {
@@ -15,6 +16,13 @@ class BuysomegamesJacksonModuleTest extends WordSpec with Matchers {
         val date = LocalDate.parse("2007-12-03")
         val result = objectMapper.writeValueAsString(date)
         result shouldBe "\"2007-12-03\""
+      }
+    }
+    "ObjectId is used" should {
+      "serialize it as hex string" in {
+        val objectId: ObjectId = new ObjectId("5894944f5591d01832058cda")
+        val result = objectMapper.writeValueAsString(objectId)
+        result shouldBe "\"5894944f5591d01832058cda\""
       }
     }
   }
